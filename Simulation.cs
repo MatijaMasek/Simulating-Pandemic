f94abb5139385342ae004fd61cd84d7a7e95a18e
MatijaMasek <58993276+MatijaMasek@users.noreply.github.com>
2020-08-27 22:06:35 +0200

    Add files via upload

---
+++ Simulation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+
+    [Header("Subjects")]
+    [SerializeField] GameObject subjectPrefab;
+    [SerializeField] GameObject infectorPrefab;
+    [SerializeField] int subjectNumber = 50;
+    [SerializeField] int infectorNumber = 1;
+
+
+    void Start()
+    {
+        GenerateSubjects();
+    }
+
+    void GenerateSubjects()
+    {
+        for (int i = 0; i < subjectNumber; i++)
+        {
+            GameObject subject = Instantiate(subjectPrefab);
+
+            subject.transform.position = getRandomPosition();
+        }
+
+        for (int i = 0; i < infectorNumber; i++)
+        {
+            GameObject infector = Instantiate(infectorPrefab);
+
+            infector.transform.position = getRandomPosition();
+        }
+
+        Vector3 getRandomPosition()
+        {
+            float x = Random.Range(-49, 49);
+            float z = Random.Range(49, -49);
+
+            return new Vector3(x, 0.5f, z);
+        }
+
+    }
+}
---
+++ Subject.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subject : MonoBehaviour
+{
+    [Header("Materials")]
+    [SerializeField] Material sick;
+    [SerializeField] Material immune;
+
+    [SerializeField] float maxSpeed = 5f;
+
+    [SerializeField] float changeTime = 2f;
+
+    [SerializeField] [Range(0f, 100f)] float chance;
+
+    [SerializeField] int timeOfRecovery = 10;
+
+    bool wasInfected = false;
+
+    bool isSick = false;
+    bool isImmune = false;
+
+    Renderer rend;
+
+    private float timeLeft;
+
+    Rigidbody rb;
+
+    Vector3 movement;
+
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+        {
+            movement = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+
+            timeLeft += changeTime;
+        }
+
+        rb.AddForce(movement * maxSpeed);
+
+        if(isSick && !wasInfected)
+        {
+            StartCoroutine(Recovery());
+        }
+        
+        if(isImmune)
+        {
+            rend.material = immune;
+        }
+    }
+
+
+    IEnumerator Recovery()
+    {
+        wasInfected = true;
+        
+        yield return new WaitForSeconds(timeOfRecovery);
+
+        float randomNumber = Random.Range(0f, 100f);
+
+        if (randomNumber < chance) 
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            isImmune = true;
+            isSick = false;
+
+            gameObject.tag = "Immune";
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sick" && !isImmune)
+        {
+            gameObject.tag = "Sick";
+
+            rend.material = sick;
+
+            isSick = true;
+            
+        }
+
+        if(collision.gameObject.tag == "Wall")
+        {
+            movement = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+        }
+    }
+
+  
+}
